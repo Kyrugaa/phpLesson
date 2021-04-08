7e7acd43c0786e65509b27d56c76bacc762a676c
Kyrugaa <martijoel@hotmail.com>
2021-04-08 13:57:51 +0200

    Lession 3, print out stuff on localhost

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
-    echo "Hi there!"
+
+/* Lession 3, print out stuff
+    echo "Hi there! ";   //echo is faster than print
+    print "Github ";
+    echo 7;             //prints out the number. No Quotationsmarks needed
+    echo 10 + 5;        //Simple Math functions.
+*/
+
+
+
 ?>
+    
+</body>
+</html>
+
+
