422b480ce25506fe7c5cc09eebd093f7266c8251
Kyrugaa <martijoel@hotmail.com>
2021-04-08 14:15:09 +0200

    Lesson 4, Variables

--- index.php
+++ index.php
@@ -9,13 +9,28 @@
 </head>
 <body>
 
+<!--
+<form method="Get">                     //Create Method
+    <input type="Text" name="person">   //input will be safed in Variable "name"   
+    <button>SUBMIT</button>             //Create Button named SUBMIT
+</form>
+-->
+
+
 <?php
 
 /* Lession 3, print out stuff
+
     echo "Hi there! ";   //echo is faster than print
     print "Github ";
     echo 7;             //prints out the number. No Quotationsmarks needed
     echo 10 + 5;        //Simple Math functions.
+*/
+/* Lession 4, Variables 
+    $name = "Kyrugaa ";         //Create Variable
+    echo $name;                 //Print out Variable
+    $name = $_GET['person'];    //Variable name is now equal to the input "person"
+    echo $name." is busy";      //Combine Variable with String
 */
 
 
