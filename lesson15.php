9d25e7b8d74aa9e3e50c4c905b536fd475653182
Kyrugaa <martijoel@hotmail.com>
2021-04-19 18:00:51 +0200

    Calculator Code done

--- lesson15.php
+++ lesson15.php
@@ -11,7 +11,43 @@
 <form >
     <input type="text" name="num1" placeholder="Number 1">
     <input type="text" name="num2" placeholder="Number 2">
+    <select name="operator">
+        <option>None</option>
+        <option>Add</option>
+        <option>Subtract</option>
+        <option>Multiply</option>
+        <option>Devide</option>
+    </select>
+    <br>
+    <button type="submit" name="submit" value="submit">Calculate</button>
 </form>
+
+<p>The Answer is:</p>
+
+<?php
+    if (isset($_GET['submit'])){
+        $result1 = $_GET['num1'];
+        $result2 = $_GET['num2'];
+        $operator = $_GET['operator'];
+        switch($operator){
+            case None:
+                echo "No operator selected";
+            break;
+            case Add:
+                echo $result1 + $result2;
+            break;
+            case Subtract:
+                echo $result1 - $result2;
+            break;
+            case Multiply:
+                echo $result1 * $result2;
+            break;
+            case Devide:
+                echo $result1 / $result2;
+            break;
+        }
+    }
+?>
 
 </body>
 </html>
